7164af4136b13eb9419a90bf8516d35efff2263f
vegildo <leobss12@gmail.com>
2022-02-25 19:53:40 -0300

    até o item 7 do cap 3

--- Cap III - Static Analysis Testing JavaScript Applications/src/example.js
+++ Cap III - Static Analysis Testing JavaScript Applications/src/example.js
@@ -1,13 +1,10 @@
-// eslint-disable-next-line strict
-'use strict'
-
-const username = 'freddy'
-typeof username === 'string'
+const username = 'freddy';
+typeof username === 'string';
 
 if (!('serviceWorker' in navigator)) {
-  // you have an old browser :-(
+    // you have an old browser :-(
 }
 
-const greeting = 'hello'
-console.log(`${greeting} world!`)
-;[1, 2, 3].forEach(x => console.log(x))
+const greeting = 'hello';
+console.log(`${greeting} world!`);
+[1, 2, 3].forEach(x => console.log(x));
