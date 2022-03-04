8776b822b5d5c9069a953ad3195d67e939c4028c
vegildo <leobss12@gmail.com>
2022-03-03 22:25:55 -0300

    item 12 cap 3

--- Cap III - Static Analysis Testing JavaScript Applications/src/typescript-example.ts
+++ Cap III - Static Analysis Testing JavaScript Applications/src/typescript-example.ts
@@ -1,12 +1,12 @@
-function add({a, b}: {a: number; b: number}): number {
-    return a + b;
+function add(a: number, b: number): number {
+    return a + b
 }
 
 interface User {
     name: {
-        first: string;
-        middle: string;
-        last: string;
+        first: string
+        middle: string
+        last: string
     };
 }
 
@@ -14,9 +14,9 @@ function getFullName(user: User): string {
     const {
         name: {first, middle, last},
     } = user;
-    return [first, middle, last].filter(Boolean).join('');
+    return [first, middle, last].filter(Boolean).join('')
 }
 
-add({a: 1, b: 2});
+add(1, 2)
 
-getFullName({name: {first: 'Leo', middle: 'Braga', last: 'souza'}});
+getFullName({name: {first: 'Leo', middle: 'Braga', last: 'souza'}})
